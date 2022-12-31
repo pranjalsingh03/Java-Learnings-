e24bcb9d92dae8e3cad73de39ac6df0abb00d3b2
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-31 22:33:35 +0530

    boolean in java

--- Main.java
+++ Main.java
@@ -5,5 +5,11 @@ public class Main {
         int c = a + b;
         System.out.println(c);
         System.out.println(10 + 20); // We can directly solve mathematical equations in print function in Java.
+        a += 10;
+        System.out.println(a);
+        System.out.println(a == b);
+        System.out.println(a != b || a == b);
+        System.out.println(a != b);
+        System.out.println(a == b && a != b);
     }
 }
