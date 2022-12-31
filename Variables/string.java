d37bc4703b249b3cd1ae4ae13572d2334d68e46d
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-31 17:59:14 +0530

    Variables in java

---
+++ Variables/string.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class string {
+    public static void main(String[] args) {
+        int m = 18;
+        String firstname = "Pranjal";
+        String lastname = "Singh";
+        String fullname = firstname + " " + lastname;
+        System.out.println("Hello World!");
+        System.out.println("My name is " + fullname);
+        System.out.println("I am " + m + " years old");
+        double num = m;
+        System.out.println("My name is " + fullname + " and I am " + m + " years old");
+        System.out.println(num);
+    }
+}
