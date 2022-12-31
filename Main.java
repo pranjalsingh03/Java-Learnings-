df55e39ede4408d4c2b5e78bd1f6f7ca6dd480dc
Pranjal-singh <pranjalsingh9304@gmail.com>
2022-12-31 17:54:27 +0530

    integers in java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+        System.out.println(10 + 20); // We can directly solve mathematical equations in print function in Java.
+    }
+}
