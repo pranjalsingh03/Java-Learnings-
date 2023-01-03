0c952bc491fda1328e83bf03375d797fa1f7b2f9
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-03 10:32:01 +0530

    replace in java

---
+++ String/Replace.java
@@ -0,0 +1,13 @@
+package String;
+
+public class Replace {
+    public static void main(String[] args) {
+
+        // replace() method
+
+        String name = "Pranjal";
+        String name2 = name.replace('a', 'e');
+        System.out.println(name); // changed string will be stored in new string
+        System.out.println(name2);
+    }
+}
