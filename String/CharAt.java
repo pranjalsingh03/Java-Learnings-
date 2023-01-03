aad4a659a2474e41d543a06549bd1bf661d70aa1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-03 10:20:33 +0530

    character at any position

---
+++ String/CharAt.java
@@ -0,0 +1,17 @@
+package String;
+
+public class CharAt {
+    public static void main(String[] args) {
+
+        // charAt() method
+
+        String name = "Pranjal";
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+        System.out.println(name.charAt(3));
+        System.out.println(name.charAt(4));
+        System.out.println(name.charAt(5));
+        System.out.println(name.charAt(6));
+    }
+}
