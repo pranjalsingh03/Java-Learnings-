a398307b00c63f1edaa1c7efaa0e801b2af576e1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-03 10:18:05 +0530

    java basics

--- Math.java
+++ Math.java
@@ -1,5 +1,5 @@
 public class Math {
     public static void main(String[] args) {
-
+        System.out.println(Math.max(12, 23));
     }
 }
---
+++ String/length.java
@@ -0,0 +1,13 @@
+package String;
+
+public class length {
+    public static void main(String[] args) {
+
+        // length of string
+
+        String name = "Pranjal";
+        String name2 = "Magan";
+        System.out.println(name.length());
+        System.out.println(name2.length());
+    }
+}
