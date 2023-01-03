2f10b67c4b90130a778722ef321b88f2993e819b
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-03 10:28:08 +0530

    concatenated in java

---
+++ String/Concatenated.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Concatenated {
+    public static void main(String[] args) {
+        String name = "Pranjal";
+        String name2 = "Magan";
+        String name3 = name + name2;
+        String name4 = name + " " + name2;
+        String name5 = name + " " + name2 + " " + "is my name";
+        System.out.println(name3);
+        System.out.println(name4);
+        System.out.println(name5);
+    }
+}
