0f93d11e5e587da85e74ea1e903a1752c313d076
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-04 21:04:34 +0530

    else if conditionals

---
+++ if_else/elseif.java
@@ -0,0 +1,15 @@
+package if_else;
+
+public class elseif {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        } else if (x < y) {
+            System.out.println("x is less than y");
+        } else {
+            System.out.println("x is equal to y");
+        }
+    }
+}
