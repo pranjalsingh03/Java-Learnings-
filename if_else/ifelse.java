696c6917ca625cec04d9fd462075aed581e9dd13
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-04 21:03:48 +0530

    if else conditional

---
+++ if_else/ifelse.java
@@ -0,0 +1,11 @@
+package if_else;
+
+public class ifelse {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+    }
+}
