324326c97568e8cf674f197e570d9275ed922903
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-04 20:47:45 +0530

    substring in java

---
+++ String/Substring.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Substring {
+    public static void main(String[] args) {
+
+        // substring() method
+
+        String name = "Magan and Pranjal";
+        String name2 = name.substring(0, 5);
+        String name3 = name.substring(10, 17);
+        System.out.println(name); // changed string will be stored in new string
+        System.out.println(name2);
+        System.out.println(name3);
+    }
+}
