bc951cec880c39b8db44624c8a5295c33f39ea69
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-07 09:45:19 +0530

    mathss in java

--- mathsss.java
+++ mathsss.java
@@ -2,6 +2,38 @@
 
 public class mathsss {
     public static void main(String[] args) {
+
+        // Returns Maximum Number
+
         System.out.println(Math.max(2, 3));
+
+        // Returns Minimum Number
+
+        System.out.println(Math.min(2, 3));
+
+        // Returns Square root of the Number
+
+        System.out.println(Math.sqrt(4));
+
+        // Returns Absoulte Positive Number
+
+        System.out.println(Math.abs(-4.7));
+
+        // Returns Smallest Number
+
+        System.out.println(Math.ceil(4.3));
+
+        // Returns Largest Number
+
+        System.out.println(Math.floor(4.3));
+
+        // Returns Power to of the Number
+
+        System.out.println(Math.pow(2, 3));
+
+        // Returns Random Number
+
+        System.out.println(Math.random());
+
     }
 }
