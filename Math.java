66ccd7d43868d8fd10b706665022467f95343d9f
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-07 09:35:16 +0530

    mathss in java

--- Math.java
+++
@@ -1,5 +0,0 @@
-public class Math {
-    public static void main(String[] args) {
-        System.out.println(Math.max(12, 23));
-    }
-}
---
+++ mathsss.java
@@ -0,0 +1,7 @@
+// Description: This program shows how to use the Math class.
+
+public class mathsss {
+    public static void main(String[] args) {
+        System.out.println(Math.max(2, 3));
+    }
+}
