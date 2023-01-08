3f159a8dfba1e8d898e91a14702265c1617f8c9c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-08 17:33:14 +0530

    switches in java

--- if_else/switchs.java
+++ if_else/switchs.java
@@ -2,4 +2,35 @@ package if_else;
 
 public class switchs {
 
+    // Insted of writing if else several times we uses switches
+    public static void main(String[] args) {
+
+        int day = 4;
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day range");
+        }
+    }
 }
