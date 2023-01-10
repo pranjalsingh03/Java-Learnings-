ba3c5d528312329510ac5c9966bcd2e25bd602c5
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-10 13:28:12 +0530

    while loop in java

--- Loop/whileloop.java
+++ Loop/whileloop.java
@@ -1,5 +1,11 @@
 package Loop;
 
 public class whileloop {
-
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5) {
+            System.out.println("The value of x is: " + x);
+            x++;
+        }
+    }
 }
