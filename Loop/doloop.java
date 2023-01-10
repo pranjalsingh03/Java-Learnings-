0717e3d883a5e450d986a31110b715c570784c7b
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-10 14:05:12 +0530

    Do loop in java

---
+++ Loop/doloop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+import Variables.string;
+
+public class doloop {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println("The value of x is: " + x);
+            x++;
+        } while (x < 9);
+    }
+}
