c79e478c7d97083cb1d277202240d18dc72ca0f1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-26 18:09:49 +0530

    for loop

--- Loop/forloop.java
+++ Loop/forloop.java
@@ -1,5 +1,9 @@
 package Loop;
 
 public class forloop {
-
+    public static void main(String[] args) {
+        for (int x = 0; x <= 5; x = x + 1) {
+            System.out.println(x);
+        }
+    }
 }
