5c03f85e9b9f7bd4e710ce2e82857a8d3761cbca
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-01-28 23:56:51 +0530

    for each loop

--- Loop/foreach.java
+++ Loop/foreach.java
@@ -1,5 +1,10 @@
 package Loop;
 
 public class foreach {
-
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
 }
