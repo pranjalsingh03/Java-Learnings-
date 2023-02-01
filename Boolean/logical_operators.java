ce5a84ecbd2d58d790c1a9dac231f2fc69a99d1f
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-01 21:06:49 +0530

    logical operators

--- Boolean/logical_operators.java
+++ Boolean/logical_operators.java
@@ -4,6 +4,9 @@ public class logical_operators {
     public static void main(String args[]) {
         boolean a = true;
         boolean b = false;
+        boolean b1 = a & b; // Logical AND
+
+        System.out.println("a&b is: " + b1); // false
         System.out.println("a&&b is: " + (a && b)); // false
         System.out.println("a||b is: " + (a || b)); // true
         System.out.println("!(a&&b) is: " + !(a && b)); // true
