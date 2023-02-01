6f6826eac55c35a01e3131cb9d79e811fdfb22af
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-01 21:10:08 +0530

    logical operators

--- Boolean/logical_operators.java
+++ Boolean/logical_operators.java
@@ -5,7 +5,14 @@ public class logical_operators {
         boolean a = true;
         boolean b = false;
         boolean b1 = a & b; // Logical AND
-
+        boolean b3 = a | b; // Logical OR
+        boolean b4 = a ^ b; // Logical XOR
+        boolean b5 = !b; // Logical NOT
+        System.out.println("a is: " + a);
+        System.out.println("b is: " + b);
+        System.out.println("!b is: " + b5); // false
+        System.out.println("a^b is: " + b4); // true
+        System.out.println("a|b is: " + b3); // true
         System.out.println("a&b is: " + b1); // false
         System.out.println("a&&b is: " + (a && b)); // false
         System.out.println("a||b is: " + (a || b)); // true
