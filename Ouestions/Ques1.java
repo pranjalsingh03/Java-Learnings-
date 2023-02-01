c612f91e0e5bd69ba0750e72a9c08498c2196577
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-01 21:31:35 +0530

    Question 1

--- Ouestions/Ques1.java
+++ Ouestions/Ques1.java
@@ -1,5 +1,9 @@
 package Ouestions;
 
 public class Ques1 {
-
+    public static void main(String args[]) {
+        int i = 50;
+        String str = (i > 50) ? "Hello" : (i < 50) ? "Hi" : "Bye";
+        System.out.println(str); // Bye
+    }
 }
