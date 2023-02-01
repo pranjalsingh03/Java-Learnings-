9ece846074f759c7f27851e7901314d91a7006cf
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-01 15:21:19 +0530

    relational operators

--- Boolean/relational_operators.java
+++ Boolean/relational_operators.java
@@ -1,5 +1,14 @@
 package Boolean;
 
 public class relational_operators {
-
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        System.out.println(a > b); // false
+        System.out.println(a < b); // true
+        System.out.println(a >= b); // false
+        System.out.println(a <= b); // true
+    }
 }
