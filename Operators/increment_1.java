16070b283b91e62ad2332c7d5c21fec42b13cba4
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-04 18:19:55 +0530

    operators

---
+++ Operators/increment_1.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class increment_1 {
+
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a++;
+        b--;
+        c += 7;
+        System.out.println("a is: " + a); // 2
+        System.out.println("b is: " + b); // 1
+        System.out.println("c is: " + c); // 10
+    }
+}
