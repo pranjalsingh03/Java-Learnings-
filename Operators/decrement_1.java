760ece1aa9a99ba16cbb676e97337efeac091890
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-06 19:58:37 +0530

    operators

--- Operators/decrement_1.java
+++ Operators/decrement_1.java
@@ -1,5 +1,16 @@
 package Operators;
 
 public class decrement_1 {
-    
+    public static void main(String args[]){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        a--;
+        b--;
+        c -= 7;
+        System.out.println("a is: " + a); // 2
+        System.out.println("b is: " + b); // 1
+        System.out.println("c is: " + c); //-4
+    }
+
 }
--- Operators/increment_1.java
+++ Operators/increment_1.java
@@ -1,8 +1,7 @@
 package Operators;
 
 public class increment_1 {
-
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int a = 1;
         int b = 2;
         int c = 3;
