dbb810cca31a3499bc06fd739e7c5b4140ab657c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-07 15:47:48 +0530

    simple intrest

--- Ouestions/simple_intrest.java
+++ Ouestions/simple_intrest.java
@@ -1,5 +1,16 @@
 package Ouestions;
 
+import java.util.Scanner;
+
 public class simple_intrest {
-    
+    public static void main(String args[]) {
+        Scanner myNum = new Scanner(System.in);
+        System.out.println("Enter the value of p, r, t: ");
+        int p = myNum.nextInt();
+        int r = myNum.nextInt();
+        int t = myNum.nextInt();
+        int si = (p * r * t) / 100;
+        System.out.println("Simple intrest is: " + si);
+
+    }
 }
