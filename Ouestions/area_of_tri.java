dce6598f99b1b7bbc557ec8a54a2ff0f66b58ecd
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-08 23:18:21 +0530

    area of triangle

--- Ouestions/area_of_tri.java
+++ Ouestions/area_of_tri.java
@@ -1,5 +1,10 @@
 package Ouestions;
 
 public class area_of_tri {
-    
+    public static void main(String[] args) {
+        int b = 5;
+        int h = 10;
+        int area = (b * h) / 2;
+        System.out.println("Area of triangle is: " + area);
+    }
 }
