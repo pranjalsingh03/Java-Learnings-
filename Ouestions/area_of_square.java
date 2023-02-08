df68f50a93479d94641cbaf3a8aa76bdccf98092
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-08 23:22:22 +0530

    area of square

--- Ouestions/area_of_square.java
+++ Ouestions/area_of_square.java
@@ -2,6 +2,8 @@ package Ouestions;
 
 public class area_of_square {
     public static void main(String[] args){
-        
+        int a = 5;
+        int area = a*a;
+        System.out.println("Area of square is: " + area);
     }
 }
