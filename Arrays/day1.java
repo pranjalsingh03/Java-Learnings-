5b2f93856e80005b85168710951ce91bf929e825
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-09 22:40:04 +0530

    arrays in loops

--- Arrays/day1.java
+++ Arrays/day1.java
@@ -1,10 +1,6 @@
 public class day1 {
     public static void main(String[] args){
-        int[] arr = {1, 2, 3, 4, 5};
-        int sum = 0;
-        for(int i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        System.out.println("Sum of array is: " + sum);
+        String[] car = {"volvo","hundayi", "BMW", "Ford", "Mazda"};
+        System.out.println(car[0]);
     }
 }
---
+++ Arrays/loops_in.java
@@ -0,0 +1,10 @@
+public class loops_in {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for(int i : arr){
+            sum += i;
+        }
+        System.out.println("Sum of array is: " + sum);
+    }
+}
