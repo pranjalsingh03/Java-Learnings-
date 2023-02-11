e40b99d62276adb93ce96792519f06a186e54c37
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-11 09:03:53 +0530

    Jframe practice

---
+++ JFrame_pr/practice1.java
@@ -0,0 +1,25 @@
+package JFrame_pr;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class practice1 {
+    public static void main(String args[]){
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel("JFrame By Example");  
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true); 
+
+    }
+}
