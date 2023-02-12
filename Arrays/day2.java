09a175fab1f17352759175bf0c3fad7aac5701a3
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-12 21:00:32 +0530

    arrays in java

--- Arrays/day2.java
+++ Arrays/day2.java
@@ -1,11 +1,11 @@
 public class day2 {
-        public static void main(String[] args){
-            int[] arr = {1, 2, 3, 4, 5};
-            int sum = 0;
-            for(int i : arr){
-                sum += i;
-            }
-            System.out.println("Sum of array is: " + sum);
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for(int i : arr){
+            sum += i;
         }
+        System.out.println("Sum of array is: " + sum);
     }
+}
     
