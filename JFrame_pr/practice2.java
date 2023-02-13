6f7d4c228ba98567c7679e26cf71147ebd282ebb
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-13 22:48:15 +0530

    JFrame practice day 2

--- JFrame_pr/practice2.java
+++ JFrame_pr/practice2.java
@@ -1,5 +1,13 @@
 package JFrame_pr;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class practice2 {
-    
+    public static void main(String[] args){
+        JFrame f = new JFrame("Day 2 Practice");
+        f.setSize(500, 500);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }    
 }
