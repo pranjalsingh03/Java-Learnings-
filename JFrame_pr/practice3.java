cadb8d56503a2e9f358488a15da06c5a289c276e
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-16 22:41:44 +0530

    color practice in jframe

--- JFrame_pr/practice3.java
+++ JFrame_pr/practice3.java
@@ -1,5 +1,7 @@
 package JFrame_pr;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
 
@@ -9,5 +11,6 @@ public class practice3 {
         f.setVisible(true);
         f.setSize(500,500);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().setBackground(Color.yellow);
     }
 }
