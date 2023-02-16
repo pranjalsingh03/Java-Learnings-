39753157793b7f2aaa1c8bbdcc7a5ac9d17b665c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-02-16 22:38:25 +0530

    jframe practice day3

--- JFrame_pr/practice3.java
+++ JFrame_pr/practice3.java
@@ -1,3 +1,13 @@
+package JFrame_pr;
+
+import javax.swing.*;
+
+
 public class practice3 {
-    
+    public static void main(String[] arg){
+        JFrame f = new JFrame("Practice");
+        f.setVisible(true);
+        f.setSize(500,500);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
